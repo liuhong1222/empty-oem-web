507d8885ccb5c85da68144bf51f9c3deee9a824f
sunyuan <sunx889@gmail.com>
2020-07-23 14:54:28 +0800

    替换创蓝logo

--- src/components/HeaderTitle/index.vue
+++ src/components/HeaderTitle/index.vue
@@ -3,7 +3,7 @@
         <!-- <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })"> -->
                 <h1 class="site-navbar__brand">
             <a class="site-navbar__brand-lg" href="javascript:;">
-                <img height="42" src="~@/assets/img/logo.png" alt />
+                <img height="42" src="~@/assets/img/logo2.png" alt />
             </a>
             <a class="site-navbar__brand-mini" href="javascript:;">253</a>
         </h1>
