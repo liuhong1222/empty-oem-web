308af8529f4f2279e1aec78a5b646dff7c9e5ec8
sunyuan <sunx889@gmail.com>
2020-07-24 09:01:19 +0800

    修改内页logo

--- src/components/HeaderTitle/index.vue
+++ src/components/HeaderTitle/index.vue
@@ -1,20 +1,16 @@
 <template>
-    <div class="site-navbar__header">
-        <!-- <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })"> -->
-                <h1 class="site-navbar__brand">
-            <a class="site-navbar__brand-lg" href="javascript:;">
-                <img height="42" src="~@/assets/img/logo2.png" alt />
-            </a>
-            <a class="site-navbar__brand-mini" href="javascript:;">253</a>
-        </h1>
-    </div>
+  <div class="site-navbar__header">
+    <!-- <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })"> -->
+    <h1 class="site-navbar__brand">
+      <a class="site-navbar__brand-lg" href="javascript:;">OEM</a>
+      <a class="site-navbar__brand-mini" href="javascript:;">OEM</a>
+    </h1>
+  </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        }
-    }
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
