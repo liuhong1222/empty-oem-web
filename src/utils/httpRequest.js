21277656603447a0925c4545799e627f6c8c5dc0
Erin <cailian_erin@163.com>
2021-11-26 11:41:04 +0800

    fix: update timeout

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -6,7 +6,7 @@ import qs from 'qs'
 import merge from 'lodash/merge'
 
 const http = axios.create({
-  timeout: 1000 * 30,
+  timeout: 1000 * 300,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json; charset=utf-8'
