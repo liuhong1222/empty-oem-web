38a2a4094292fd3698a3bbaa677f5fbb71a2ccd0
dengcl <dengcl@253.com>
2021-11-29 11:14:53 +0800

    fix: update config

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,13 +5,11 @@
   window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = '/open/' // stable 环境
-  // window.SITE_CONFIG['baseUrl'] = 'http://172.16.42.144:8658/open/' // stable 环境 使用绝对路径因为前后端不在同一环境下会出现跨域的问题
-  // window.SITE_CONFIG['baseUrl'] = 'http://oem.haifou.com/open/' // 线上
+  window.SITE_CONFIG['baseUrl'] = '/open/'
 
   // 资源路径
-  window.SITE_CONFIG['imgUrl'] = 'http://test.haifou.com:8088/' // stable 环境
-  window.SITE_CONFIG['zxaImgUrl'] = 'http://test.haifou.com:8088/' // stable 环境
+  window.SITE_CONFIG['imgUrl'] = 'http://test.haifou.com:8088/'
+  window.SITE_CONFIG['zxaImgUrl'] = 'http://test.haifou.com:8088/'
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
